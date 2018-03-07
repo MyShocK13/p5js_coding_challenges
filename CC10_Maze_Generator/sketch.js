a6a62b3d49a305fce0939abe72dbbaff36a5ce44
Peregrino Cao <shock.ko@gmail.com>
2018-03-07 21:29:45 +0100

    2nd part of CC10 Maze Generator

--- CC10_Maze_Generator/sketch.js
+++ CC10_Maze_Generator/sketch.js
@@ -1,6 +1,7 @@
 let cols, rows;
 let w = 40;
 let grid = [];
+let current;
 
 function setup() {
     createCanvas(400, 400);
@@ -13,6 +14,8 @@ function setup() {
             grid.push(new Cell(i, j));
         }
     }
+
+    current = grid[0];
 }
 
 function draw() {
@@ -21,14 +24,23 @@ function draw() {
     for (let i in grid) {
         grid[i].show();
     }
+
+    current.visited = true;
+    let next = current.checkNeighbours();
+
+    if (next) {
+        next.visited = true;
+        current = next;
+    }
 }
 
 function Cell(i, j) {
     this.i = i;
     this.j = j;
     this.walls = [true, true, true, true];
+    this.visited = false;
 
-    this.show = function() {
+    this.show = function () {
         let x = this.i * w;
         let y = this.j * w;
         stroke(255);
@@ -45,7 +57,45 @@ function Cell(i, j) {
             line(x, y + w, x, y);
         }
 
-        // noFill();
-        // rect(x, y, w, w);
+        if (this.visited) {
+            fill(255, 0, 255, 100);
+            rect(x, y, w, w);
+        }
     }
+
+    this.checkNeighbours = function () {
+        let neighbours = [];
+
+        let top = grid[index(i, j - 1)];
+        let right = grid[index(i + 1, j)];
+        let bottom = grid[index(i, j + 1)];
+        let left = grid[index(i - 1, j)];
+
+        if (top && !top.visited) {
+            neighbours.push(top);
+        }
+        if (right && !right.visited) {
+            neighbours.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbours.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbours.push(left);
+        }
+
+        if (neighbours.length > 0) {
+            let r = floor(random(0, neighbours.length));
+            return neighbours[r];
+        } else {
+            return undefined;
+        }
+    }
+}
+
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+        return -1;
+    }
+    return i + j * cols;
 }
