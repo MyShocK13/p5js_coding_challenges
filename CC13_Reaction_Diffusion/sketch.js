62f43a5c4ff7cb9da0b3d3d61dd46abdd2fa254b
Peregrino Cao <shock.ko@gmail.com>
2018-03-15 23:33:34 +0100

    Finished CC13 Reaction Diffusion

--- CC13_Reaction_Diffusion/sketch.js
+++ CC13_Reaction_Diffusion/sketch.js
@@ -1,8 +1,13 @@
 let grid = [];
 let next = [];
 
+const dA = 1;
+const dB = 0.5;
+const feed = 0.055;
+const k = 0.062;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(100, 100);
     pixelDensity(1);
 
     for (let x = 0; x < width; x++) {
@@ -10,22 +15,47 @@ function setup() {
         next[x] = [];
         for (let y = 0; y < height; y++) {
             grid[x][y] = {
-                a: random(1),
-                b: random(1)
-            }
-            next[x][y] = {
-                a: 0,
+                a: 1,
                 b: 0
             }
+            next[x][y] = {
+                a: 1,
+                b: 0
+            }
+        }
+    }
+
+    for (let i = 50; i < 52; i++) {
+        for (let j = 50; j < 52; j++) {
+            grid[i][j].b = 1;
+        }
+    }
+
+    for (let i = 25; i < 30; i++) {
+        for (let j = 25; j < 30; j++) {
+            grid[i][j].b = 1;
+        }
+    }
+
+    for (let i = 75; i < 87; i++) {
+        for (let j = 25; j < 30; j++) {
+            grid[i][j].b = 1;
         }
     }
 }
 
 function draw() {
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            next[x][y].a = grid[x][y].a * 0.2;
-            next[x][y].b = grid[x][y].b * 1.2;
+    background(51);
+
+    for (let x = 1; x < width - 1; x++) {
+        for (let y = 1; y < height - 1; y++) {
+            let a = grid[x][y].a;
+            let b = grid[x][y].b;
+            next[x][y].a = a + (dA * laplaceA(x, y)) - (a * b * b) + (feed * (1 - a));
+            next[x][y].b = b + (dB * laplaceB(x, y)) + (a * b * b) - ((k + feed) * b);
+
+            next[x][y].a = constrain(next[x][y].a, 0, 1);
+            next[x][y].b = constrain(next[x][y].b, 0, 1);
         }
     }
 
@@ -33,9 +63,13 @@ function draw() {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             let pix = (x + y * width) * 4;
-            pixels[pix + 0] = floor(next[x][y].a * 255);
-            pixels[pix + 1] = 0;
-            pixels[pix + 2] = floor(next[x][y].b * 255);
+            let a = next[x][y].a;
+            let b = next[x][y].b;
+            let c = floor((a - b) * 255);
+            c = constrain(c, 0, 255);
+            pixels[pix + 0] = c;
+            pixels[pix + 1] = c;
+            pixels[pix + 2] = c;
             pixels[pix + 3] = 255;
         }
     }
@@ -48,4 +82,36 @@ function swap() {
     let temp = grid;
     grid = next;
     next = temp;
+}
+
+function laplaceA(x, y) {
+    let sumA = 0;
+
+    sumA += grid[x][y].a * -1;
+    sumA += grid[x - 1][y].a * 0.2;
+    sumA += grid[x + 1][y].a * 0.2;
+    sumA += grid[x][y - 1].a * 0.2;
+    sumA += grid[x][y + 1].a * 0.2;
+    sumA += grid[x + 1][y + 1].a * 0.05;
+    sumA += grid[x + 1][y - 1].a * 0.05;
+    sumA += grid[x - 1][y + 1].a * 0.05;
+    sumA += grid[x - 1][y - 1].a * 0.05;
+
+    return sumA;
+}
+
+function laplaceB(x, y) {
+    let sumB = 0;
+
+    sumB += grid[x][y].b * -1;
+    sumB += grid[x - 1][y].b * 0.2;
+    sumB += grid[x + 1][y].b * 0.2;
+    sumB += grid[x][y - 1].b * 0.2;
+    sumB += grid[x][y + 1].b * 0.2;
+    sumB += grid[x + 1][y + 1].b * 0.05;
+    sumB += grid[x + 1][y - 1].b * 0.05;
+    sumB += grid[x - 1][y + 1].b * 0.05;
+    sumB += grid[x - 1][y - 1].b * 0.05;
+
+    return sumB;
 }
