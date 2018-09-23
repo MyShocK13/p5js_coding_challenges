0589d6495e6feb6b7446e477511d33e93e45fbf8
MyShocK13 <shock.ko@gmail.com>
2018-09-23 10:44:42 +0200

    Added CC65 Binary Search Tree

---
+++ CC65_Binary_Search_Tree/sketch.js
@@ -0,0 +1,12 @@
+let tree;
+
+function setup() {
+    createCanvas(600, 400);
+    background(51);
+    tree = new Tree();
+    for (let i = 0; i < 10; i++) {
+        tree.addValue(floor(random(0, 100)));
+    }
+    console.log(tree);
+    tree.traverse();
+}
