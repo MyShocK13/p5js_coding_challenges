193210c1e178506c22eb0d15d23d3bc3ab8e7a42
MyShocK13 <shock.ko@gmail.com>
2018-09-24 22:01:01 +0200

    Finished CC68 Bread-First Search

--- CC68_Bread-First_Search/graph.js
+++ CC68_Bread-First_Search/graph.js
@@ -2,6 +2,8 @@ class Graph {
     constructor() {
         this.nodes = [];
         this.graph = {};
+        this.start = null;
+        this.end = null;
     }
 
     addNode(n) {
@@ -13,5 +15,22 @@ class Graph {
     getNode(actor) {
         let n = this.graph[actor];
         return n;
+    }
+
+    setStart(actor) {
+        this.start = this.graph[actor];
+        return this.start;
+    }
+
+    setEnd(actor) {
+        this.end = this.graph[actor];
+        return this.end;
+    }
+
+    reset() {
+        for (let i = 0; i < this.nodes.length; i++) {
+            this.nodes[i].searched = false;
+            this.nodes[i].parent = null;
+        }
     }
 }
--- CC68_Bread-First_Search/sketch.js
+++ CC68_Bread-First_Search/sketch.js
@@ -1,5 +1,6 @@
 let data;
 let graph;
+let dropdown
 
 function preload() {
     data = loadJSON('kevin-bacon.json');
@@ -7,8 +8,11 @@ function preload() {
 
 function setup() {
     graph = new Graph();
+    dropdown = createSelect();
+    dropdown.changed(bfs);
     noCanvas();
 
+    // setting up the tree and the relationships
     let movies = data.movies;
 
     for (let i = 0; i < movies.length; i++) {
@@ -22,10 +26,56 @@ function setup() {
             let actorNode = graph.getNode(actor);
             if (actorNode == undefined) {
                 actorNode = new Node(actor);
+                dropdown.option(actor);
             }
             graph.addNode(actorNode);
             movieNode.addEdge(actorNode);
         }
     }
-    console.log(graph);
+}
+
+function bfs() {
+    graph.reset();
+    // the Brear-First Search algorithm
+    let start = graph.setStart(dropdown.value());
+    let end = graph.setEnd("Kevin Bacon");
+    let queue = []
+    start.searched = true;
+    queue.push(start);
+    while (queue.length > 0) {
+        let current = queue.shift();
+        if (current == end) {
+            console.log("Found " + current.value);
+            break;
+        }
+        let edges = current.edges;
+        for (let i = 0; i < edges.length; i++) {
+            let neighbour = edges[i];
+            if (!neighbour.searched) {
+                neighbour.searched = true;
+                neighbour.parent = current;
+                queue.push(neighbour);
+            }
+        }
+    }
+
+    // when we found, we tracking the path
+    let path = [];
+    path.push(end);
+    let next = end.parent;
+    while (next != null) {
+        path.push(next);
+        next = next.parent;
+    }
+
+    // showing the path in the browser
+    let txt = '';
+    for (let i = path.length - 1; i >= 0; i--) {
+        let n = path[i];
+        txt += n.value;
+        if (i != 0) {
+            txt += ' --> ';
+        }
+    }
+    createP(txt);
 }
